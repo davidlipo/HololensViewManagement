6f648bf2e3036e5adab61f614bc98eefa24d2d39
davidlipo <dlipowicz@hotmail.com>
2016-11-06 23:02:50 +0000

    Adds label to an object in a free space

--- Assets/TargetScript.cs
+++ Assets/TargetScript.cs
@@ -22,7 +22,7 @@ public class TargetScript : MonoBehaviour {
         }
     }
 
-    Rect getBounds()
+    public Rect getBounds()
     {
         return bounds;
     }
@@ -30,27 +30,33 @@ public class TargetScript : MonoBehaviour {
     //http://answers.unity3d.com/questions/49943/is-there-an-easy-way-to-get-on-screen-render-size.html
     public static Rect GUIRectWithObject(GameObject go)
     {
-        Vector3 cen = go.GetComponentInChildren<Renderer>().bounds.center;
-        Vector3 ext = go.GetComponentInChildren<Renderer>().bounds.extents;
-        Vector2[] extentPoints = new Vector2[8]
-         {
-               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z-ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z-ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z+ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z+ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z-ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z-ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z+ext.z)),
-               WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z+ext.z))
-         };
-        Vector2 min = extentPoints[0];
-        Vector2 max = extentPoints[0];
-        foreach (Vector2 v in extentPoints)
+        if (go)
         {
-            min = Vector2.Min(min, v);
-            max = Vector2.Max(max, v);
+            Vector3 cen = go.GetComponentInChildren<Renderer>().bounds.center;
+            Vector3 ext = go.GetComponentInChildren<Renderer>().bounds.extents;
+            Vector2[] extentPoints = new Vector2[8]
+             {
+                   WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z-ext.z)),
+                   WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z-ext.z)),
+                   WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y-ext.y, cen.z+ext.z)),
+                   WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y-ext.y, cen.z+ext.z)),
+                   WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z-ext.z)),
+                   WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z-ext.z)),
+                   WorldToGUIPoint(new Vector3(cen.x-ext.x, cen.y+ext.y, cen.z+ext.z)),
+                   WorldToGUIPoint(new Vector3(cen.x+ext.x, cen.y+ext.y, cen.z+ext.z))
+             };
+            Vector2 min = extentPoints[0];
+            Vector2 max = extentPoints[0];
+            foreach (Vector2 v in extentPoints)
+            {
+                min = Vector2.Min(min, v);
+                max = Vector2.Max(max, v);
+            }
+            return new Rect(min.x, min.y, max.x - min.x, max.y - min.y);
+        } else
+        {
+            return new Rect();
         }
-        return new Rect(min.x, min.y, max.x - min.x, max.y - min.y);
     }
 
     public static Vector2 WorldToGUIPoint(Vector3 world)
--- Assets/TrackingManagerScript.cs
+++ Assets/TrackingManagerScript.cs
@@ -1,27 +1,103 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class TrackingManagerScript : MonoBehaviour {
 
     private GameObject[] trackedObjs;
+    private int width;
+    private int height;
+    private int[,] pixels;
+    public Texture aTexture;
 
-	// Use this for initialization
-	void Start () {
-	
+    // Use this for initialization
+    void Start () {
+        width = Screen.width;
+        height = Screen.height;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update() {
         trackedObjs = GameObject.FindGameObjectsWithTag("TrackedObj");
+
+        pixels = generatePixelMap();
     }
 
-    /*void OnGUI()
+    int[,] generatePixelMap()
     {
-        if (isOnScreen && aTexture)
+        List<Rect> trackedRects = new List<Rect>();
+        int[,] pixels = new int[height, width];
+
+        for (int k = 0; k < trackedObjs.Length; k++)
         {
-            //GUI.DrawTexture(new Rect(screenPos.x, Screen.height - screenPos.y, 60, 60), aTexture);
-            GUI.color = new Color(1.0f, 1.0f, 1.0f, 0.25f);
-            GUI.DrawTexture(bounds, aTexture);
+            trackedRects.Add(trackedObjs[k].GetComponent<TargetScript>().getBounds());
         }
-    }*/
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Vector2 point = new Vector2(j, i);
+                pixels[i, j] = 0;
+                foreach (Rect rect in trackedRects)
+                {
+                    if (rect.Contains(point))
+                    {
+                        pixels[i, j] = 1;
+                        break;
+                    }
+                }
+            }
+        }
+        return pixels;
+    }
+
+    Vector2 placeLabel(int aimHeight, int aimWidth, Vector2 objLoc)
+    {
+        int noColumns = Mathf.FloorToInt(width / aimWidth);
+        int noRows = Mathf.FloorToInt(height / aimHeight);
+
+        int[] fullnessOfBlock = new int[noColumns * noRows];
+        int emptiestBlock = 0;
+        int emptiestValue = aimHeight * aimWidth + 1;
+
+        for (int i = 0; i < noRows; i++)
+        {
+            for (int j = 0; j < noColumns; j++)
+            {
+                int inBlock = 0;
+                for (int k = i * aimHeight; k < (i + 1) * aimHeight; k++)
+                {
+                    for (int l = j * aimWidth; l < (j + 1) * aimWidth; l++)
+                    {
+                        inBlock += pixels[k, l];
+                    }
+                }
+                int blockNo = i * noColumns + j;
+                int distanceFromObjX = Mathf.Abs(Mathf.FloorToInt(objLoc.x / aimWidth) - j);
+                int distanceFromObjY = Mathf.Abs(Mathf.FloorToInt(objLoc.y / aimHeight) - i);
+                inBlock += distanceFromObjX * aimWidth + distanceFromObjY * aimHeight;
+                fullnessOfBlock[blockNo] = inBlock;
+                if (inBlock < emptiestValue)
+                {
+                    emptiestValue = inBlock;
+                    emptiestBlock = blockNo;
+                }
+            }
+        }
+
+        int emptiestBlockColumn = emptiestBlock % noColumns;
+        int emptiestBlockRow = emptiestBlock / noColumns;
+
+        return new Vector2(emptiestBlockColumn * aimWidth, emptiestBlockRow * aimHeight);
+    }
+
+    void OnGUI()
+    {
+        Rect rect = trackedObjs[0].GetComponent<TargetScript>().getBounds();
+        Vector2 locationToUse = placeLabel(100, 100, rect.center);
+        GUI.DrawTexture(new Rect(locationToUse.x, locationToUse.y, 100, 100), aTexture);
+        //GUI.color = new Color(1.0f, 1.0f, 1.0f, 0.25f);
+        //GUI.DrawTexture(bounds, aTexture);
+    }
 }
